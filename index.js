b22940091aa8edde56fffc9ecf4c6a8ebde7100a
Maciek Wróblewski <subskrypcje.maciek@gmail.com>
2021-01-13 14:54:57 +0100

    change class form and add script prepare to add function to send post to mailerlite after fomrs validation

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-function terms_changed(termsCheckBox){
-    //If the checkbox has been checked
-    if(termsCheckBox.checked){
-        //Set the disabled property to FALSE and enable the button.
-        document.getElementById("thankYouRedirect").disabled = false;
-    } else{
-        //Otherwise, disable the submit button.
-        document.getElementById("thankYouRedirect").disabled = true;
+$('#form').submit(function (event) {
+    event.preventDefault();
+    if ($('#form')[0].checkValidity() === false) {
+        event.stopPropagation();
+        console.log("nie")
+    
+    } else {
+        console.log("jest")
+        $('#newsletterForm').modal('hide')
     }
-}
-
+    $('#form').addClass('was-validated');
+});
