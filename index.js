20c4e01cb985622bed306fe574522becd633a22b
Maciek Wróblewski <subskrypcje.maciek@gmail.com>
2021-01-15 13:38:49 +0100

    take value form input and prepare to sen post req'

--- index.js
+++ index.js
@@ -5,8 +5,36 @@ $('#form').submit(function (event) {
         console.log("nie")
     
     } else {
-        console.log("jest")
+        var userEmail = document.getElementById('inputEmail').value;
+        var userName = document.getElementById('inputName').value;
+
+        var request = {
+            "url": "https://api.mailerlite.com/api/v2/subscribers",
+            "method": "POST",
+            "timeout": 0,
+            "headers": {
+              "Content-Type": "application/json",
+              "X-MailerLite-ApiKey": "e1948ef8407a0e01ee89bb758753135b",
+              "Cookie": "__cfduid=d7243adbed2c3fae1d7ef42b0c926bcee1609412932"
+            },
+            "data": JSON.stringify({"email": userEmail,"name": userName}),
+          };
+          
+         /* $.ajax(request).done(function (response) {
+            console.log(response);
+          });*/
+        console.log(request)
+        clearInputs('inputEmail', 'inputName');
+        
+        
         $('#newsletterForm').modal('hide')
+        
     }
     $('#form').addClass('was-validated');
 });
+
+function clearInputs(firstInpuId, secondInputId){
+    document.getElementById(firstInpuId).value=null;
+    document.getElementById(secondInputId).value='';
+
+}
